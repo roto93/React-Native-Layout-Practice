2e263f6ddff77370a1a96a1052307d9ecef5cc22
roto93 <roto93@yahoo.com.tw>
2021-06-27 16:48:31 +0800

    init

--- App.js
+++ App.js
@@ -1,33 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import AppLoading from 'expo-app-loading';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import StartScreen from './src/Screens/StartScreen/StartScreen';
-import MainScreen from './src/Screens/MainScreen/MainScreen';
-import { useFonts, Roboto_400Regular, Roboto_300Light, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Stack = createStackNavigator();
+
 
 function App() {
-  const [fontLoaded] = useFonts({
-    Roboto_300Light,
-    Roboto_400Regular,
-    Roboto_500Medium
-  })
-  const [start, setStart] = useState(false);
-  if (!fontLoaded) return <AppLoading />
-  else if (!start) return <StartScreen setIsStart={setStart} />
-  else return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerLeft: () => <TouchableOpacity onPress={() => { setStart(false) }} style={{ width: 50, height: 50, backgroundColor: 'gold' }}></TouchableOpacity> }}
-      >
-        <Stack.Screen name={'Main'} component={MainScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (
+    <View style={styles.container}>
+      <Text>Hi</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default App
