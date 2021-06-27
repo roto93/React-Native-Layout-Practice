cc028d04b7c45552ded9123b65e068329440945c
roto93 <roto93@yahoo.com.tw>
2021-06-27 16:37:32 +0800

    6/27 startPage

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import StartScreen from './src/Screens/StartScreen/StartScreen';
+import MainScreen from './src/Screens/MainScreen/MainScreen';
+import { useFonts, Roboto_400Regular, Roboto_300Light, Roboto_500Medium } from '@expo-google-fonts/roboto';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const Stack = createStackNavigator();
+
+function App() {
+  const [fontLoaded] = useFonts({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium
+  })
+  const [start, setStart] = useState(false);
+  if (!fontLoaded) return <AppLoading />
+  else if (!start) return <StartScreen setIsStart={setStart} />
+  else return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerLeft: () => <TouchableOpacity onPress={() => { setStart(false) }} style={{ width: 50, height: 50, backgroundColor: 'gold' }}></TouchableOpacity> }}
+      >
+        <Stack.Screen name={'Main'} component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ src/Screens/MainScreen/MainScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MainScreen = () => {
+    return (
+        <View>
+            <Text>Main Page</Text>
+        </View>
+    )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({
+
+})
